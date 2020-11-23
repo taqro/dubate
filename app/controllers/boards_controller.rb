fe2fcccf6f500adf2ce824348d78055196390c66
taqro <takurousaitou94@yahoo.co.jp>
2020-11-23 13:24:13 +0900

    Changing Board create action

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -1,4 +1,12 @@
 class BoardsController < ApplicationController
   def show
+
+  end
+
+  def create
+    @room = Room.find(params[:room_id])
+    @board = @room.build_board(user_id: current_user.id)
+    @board.save
+    redirect_to room_boards_path(@room, @board)
   end
 end
