2a23ca9932e8bc563860736d2e26875ea6af511b
taqro <takurousaitou94@yahoo.co.jp>
2020-11-26 15:42:05 +0900

    Refactoring user_profile view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,28 +1,39 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!
-  helper_method :debating_number
+  helper_method :debating_number, :waiting_number, :participated_rooms
 
 
   def show
     @user = User.find(params[:id]) ? User.find(params[:id]) : current_user
     @rooms = @user.rooms.order(:id)
     @lose_number = @user.boards.count
-    @win_number = match_number - @lose_number - debating_number
-    @matched_number = match_number - debating_number #総対戦数
+    @win_number = match_number - @lose_number - debating_number-waiting_number-debating_number
+    @matched_number = match_number - debating_number-waiting_number #総対戦数
   end
 
   # 議論中のRoom数
   def debating_number
     @debating_number = 0
     participated_rooms.each do |room|
-      if room.board.nil?
+      if room.board.nil? and !(room.opponent_id.nil?)
         @debating_number += 1
       end
     end
     return @debating_number
   end
 
-  private
+  # 相手を探し中のRoom数
+  def waiting_number
+    open_rooms = @user.rooms
+    @waiting_number = 0
+    open_rooms.each do |room|
+      if room.opponent_id.nil?
+        @waiting_number += 1
+      end
+    end
+    return @waiting_number
+  end
+
   # 参加したRoom
   def participated_rooms
     participated_rooms = Room.where(user_id: @user.id) + Room.where(opponent_id: @user.id)
