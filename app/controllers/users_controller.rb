cadc382cc1cda28307327efe8f981ab19de5f7d8
taqro <takurousaitou94@yahoo.co.jp>
2022-02-25 13:11:38 +0900

    フォロー機能の作成（一覧表示以外）

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,19 @@
+class UsersController < ApplicationController
+  def show
+    @user = User.find_by(id: params[:id])
+  end
+
+  def following
+    @title = "Following"
+    @user = User.find(params[:id])
+    @users = @user.following.paginate(page: params[:page])
+    render 'show_follow'
+  end
+
+  def followers
+    @title = "Followers"
+    @user  = User.find(params[:id])
+    @users = @user.followers.paginate(page: params[:page])
+    render 'show_follow'
+  end
+end
