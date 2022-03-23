7c87b72fc56e70bd5cf647ce7ec17b0b4b74fac2
taqro <takurousaitou94@yahoo.co.jp>
2022-03-23 11:48:26 +0900

    議論の削除機能の作成

--- app/controllers/debates_controller.rb
+++ app/controllers/debates_controller.rb
@@ -12,12 +12,15 @@ class DebatesController < ApplicationController
 
   def create
     debate = current_user.create_debates.build(debate_params)
-    debate.joined_user_id = 1
+    debate.joined_user_id = 1 #validationを通すために一時的に適当な値を入れている  後で直す。
     debate.save!
     redirect_to debates_path
   end
 
   def destroy
+    debate = Debate.find(params[:id])
+    debate.destroy
+    redirect_to debates_url
   end
 
   def edit
