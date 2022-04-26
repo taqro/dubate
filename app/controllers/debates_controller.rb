600902f0c65d854418280640b08f1d96639350f7
taqro <takurousaitou94@yahoo.co.jp>
2022-04-26 11:31:59 +0900

    rootのページを消していたので、その修正

--- app/controllers/debates_controller.rb
+++ app/controllers/debates_controller.rb
@@ -1,6 +1,6 @@
 class DebatesController < ApplicationController
 
-  before_action :authenticate_user! except: [:index, :show]
+  before_action :authenticate_user!, except: [:index, :show]
 
   def index
     @q = Debate.ransack(params[:q]) #ransack(検索のgem)
